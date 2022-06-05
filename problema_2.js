9fa6de2c2d9bd568c1fe9bd4692263cbb706624e
Braulio <b.oyarzun.barros@gmail.com>
2022-06-05 12:49:29 -0400

    destructuration js

---
+++ problema_2.js
@@ -0,0 +1,13 @@
+const employee = {
+  name: 'Elon',
+  age: 47,
+  company: 'Tesla'
+}
+const { name: otherName } = employee;
+
+//Predict the output
+console.log(name);
+// name no está definido
+
+console.log(otherName);
+// Elon
